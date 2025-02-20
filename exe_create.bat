bbf1b89ffd5d760721abf70b0396592d68a3be49
nhathuynguyen <huythcsthuyphuong73@gmail.com>
2025-02-20 17:57:27 +0700

    update exe_create.bat to build mfosync.exe instead of app.exe

--- exe_create.bat
+++ exe_create.bat
@@ -1,10 +1,11 @@
 @echo off
 cd /d "%~dp0" 
-pyinstaller --onefile --noconsole app.py
-for /f "delims=" %%i in ('where /r "%cd%\dist" app.exe') do set EXE_PATH=%%i
+del /q mfosync.exe
+pyinstaller --onefile --noconsole mfosync.py
+for /f "delims=" %%i in ('where /r "%cd%\dist" mfosync.exe') do set EXE_PATH=%%i
 if exist "%EXE_PATH%" (
     move /Y "%EXE_PATH%" "%cd%"
 )
 rmdir /s /q dist
-del /q app.spec
+del /q mfosync.spec
 pause
